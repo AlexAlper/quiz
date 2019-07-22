09cdaa3c8eebd5895019941ac49c113226012840
Alex <alexanderperepechaev@gmail.com>
2019-07-22 07:52:27 +0300

    Теперь правильных ответов может быть сколько угодно. Добавил еще вопросов, теперь их 10

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 
 let list = [
+   
     {1: 'Первый человек в космосе?', 2: 5,  3: 'Армстронг', 4: 'Громов', 5: 'Гагарин'},
     {1: 'Самое быстрое млекопитающее?', 2:4, 3: 'Вилорог', 4: 'Гепард', 5: 'Ягуар'},
     {1: 'Лучший язык программирования?', 2: 3, 3: 'Js', 4: 'С#', 5: 'PHP'},
-    {1: 'Четные числа', 2: 34, 3: '2', 4:'6', 5:'7', 6:'9' }
-    
+    {1: 'Четные числа:', 2: 34, 3: '2', 4:'6', 5:'7', 6:'9' },
+    {1: 'Нечетные числа:', 2:457, 3:'4', 4:'7', 5:'9',6:'12', 7:'13'},
+    {1: 'Количество планет в солнечной системе?', 2:4, 3:'7', 4:'8', 5:'9', 6:'10'},
+    {1: 'Количество регионов в России', 2:3, 3:'85', 4:'86', 5:'88', 6:'89'},
+    {1: 'Простые числа:', 2: 35, 3:'5', 4:'6', 5:'7', 6:'8', 7:'9'},
+    {1: 'Самая яркая звезда на ночном небе(за исключением Солнца)?', 2:5, 3:'Арктур', 4:'Альфа Центавра', 5:'Сириус', 6:'Канопус'},
+    {1: 'Площадь Луны км²', 2:'4', 3:'2,793*10000000', 4:'3,793*10000000', 5: '2,793*1000000', 6:'3,793*1000000'}
+
 ];
 
 
 
-let ans = document.querySelector('.answer');
-    question = document.querySelector('.question');
-    nextBtn = document.querySelector('.next');
-    endBtn = document.querySelector('.end');
+let ans = document.querySelector('.answer');                    //блок ответов
+    question = document.querySelector('.question');             //вопрос
+    nextBtn = document.querySelector('.next');                  //кнопка следующего вопроса
+    endBtn = document.querySelector('.end');                    //кнопка окончания теста
     parentQuestion = document.getElementById('question');
-    progresbar = document.getElementById('progressbar');
-    startBlock = document.querySelector('.start-quiz');
-    startBtn = document.querySelector('.start-btn');
-    checkbox = document.querySelector('.form-check-input');
-    countAnswer = 0;
-    tmpTrueAnswer = 0;
-    arr = [];
-    touchElem = 0;
+    progresbar = document.getElementById('progressbar');        //прогресс бар
+    startBlock = document.querySelector('.start-quiz');         //стартовый блок
+    startBtn = document.querySelector('.start-btn');            //кнопка старта
+    checkbox = document.querySelector('.form-check-input');     //выбор выводить ответ стразу или только в конце теста
+    countAnswer = 0;                                            //количесство ответов в текущем вопросе
+    tmpTrueAnswer = 0;                                          //количество верных ответов в текущем вопросе
+    arr = [];                                                   //массив верных ответов в текущем вопросе
 
     step = 100/list.length;
     count = 0;
@@ -44,6 +50,7 @@ startBtn.addEventListener('click', function(){
 
 });
 
+//Создает форму вопроса
 function constructorQuestions(){
     //document.body.style.background = 'url(img/' + count + '.jpg)';
     for (var key in list[count]) {
@@ -77,18 +84,19 @@ answer.forEach(function(item) {
 
     item.addEventListener('click', function(e){
         let target = e.target;
-        console.log(touchElem + ' ' + target.id);
-        if(!nextQuest && touchElem != target.id){
+
+        if(!nextQuest && target.style.background == ''){
+
             if(checkboxValue){
         for(let elem = 0; elem < arr.length; elem++){
             if(target.id == arr[elem]-1){ 
                 target.style.background = '#008000';
                 tmpTrueAnswer++;
-                touchElem = arr[elem]-1;
+
                 break;
             } else {
                 target.style.background = '#ff0000';
-                console.log(target);  
+
             }
         }
        
